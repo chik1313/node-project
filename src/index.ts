a1ffe3728b4df886ced5848b40d75f74005150ef
chik1313 <ochkurov96@gmail.com>
2024-12-09 19:31:04 +0100

    add some updates

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,37 @@
-import express , {Request , Response} from "express";
+import express, {Request, Response} from "express";
 
 
 const app = express()
 const port = 3000
 
-app.get('/courses', (req:Request, res:Response) => {
-  res.json([
-      { id: 1 , title: 'front-end' },
-      { id: 2 , title: 'back-end' },
-      { id: 3 , title: 'automation - qa' },
-      { id: 4 , title: 'dev-ops' },
-  ])
+const db = {
+    courses: [
+        {id: 1, title: 'front-end'},
+        {id: 2, title: 'back-end'},
+        {id: 3, title: 'automation - qa'},
+        {id: 4, title: 'dev-ops'},
+    ]
+}
+
+app.get('/courses', (req: Request, res: Response) => {
+    const findedCourses = db.courses.filter(c => c.title.indexOf(req.query.title as string) > -1)
+    res.json(findedCourses)
 })
-app.get('/courses/:id', (req:Request, res:Response) => {
-  res.json([
-      { id: 1 , title: 'front-end' },
-      { id: 2 , title: 'back-end' },
-      { id: 3 , title: 'automation - qa' },
-      { id: 4 , title: 'dev-ops' },
-  ].find(c => c.id === +req.params.id))
+
+
+app.get('/courses/:id', (req: Request, res: Response) => {
+    const findedCourse = db.courses.find(c => c.id === +req.params.id)
+
+    if (!findedCourse) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.json(findedCourse)
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
+
